64b0762fba6366244fd2fb07d436853f7e9a2165
geronimord10 <geronimorodriguez79@gmail.com>
2023-09-28 15:43:41 -0300

    se agrego validacion en dexter

--- cypress/e2e/step_definitions/dexter.js
+++ cypress/e2e/step_definitions/dexter.js
@@ -1,5 +1,6 @@
 import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
-const claroHome = require("../../pages/homeClaro");
+const dexter = require("../../pages/dexter");
+
 
 When(`clickea la opción {string}`, (txt) => {
     cy.contains(txt).click();
@@ -7,4 +8,22 @@ When(`clickea la opción {string}`, (txt) => {
 
 When(`realiza scroll hasta {string}`, (text) => {
     cy.contains(text).scrollIntoView();
+});
+
+When(`selecciona la opcion de {string}`, (opcion) => {
+    dexter.getBrandLink().each((brand) => {
+        cy.wrap(brand).should("have.attr", "href").then((textoHref) => {
+            cy.log(`**${textoHref}`);
+
+            if (textoHref.includes(opcion)) {
+                cy.wrap(brand).click({ force: true });
+            } else {
+                dexter.getRightArrow().first().click({ force: true })
+            }
+        });
+    });
+});
+
+Then(`se verifica que la url contiene {string}`, (url) => {
+    cy.url().should("include", url)
 });
